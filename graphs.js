f227952fd4888b08c259487af07ba52c157a53bd
Dylan Moberg <dylanmoberg11@gmail.com>
2016-08-03 20:41:34 -0700

    finished chapter on graphs

---
+++ graphs.js
@@ -0,0 +1,21 @@
+(function() {
+    //abstract model of a network structure
+
+    //set of nodes connected by edges
+    //any binary relationship can be represented in a graph
+
+    //social networks can also be represented by graphs
+
+    // graph G = (v, e)
+    //composed of V: a set of vertices
+    //            E: A set of edges connecting the vertices in V
+
+    //vertices connected by an edge are adjacent vertices
+    //A degree of a vertex consits of the number of adjacent vertices
+    // A path is a sequence of consecutive vertices
+    //simple path does not include repeated vertices
+    //undirected or directed graphs
+    //graphs can be weighted or unweighted
+
+
+})('use strict');
